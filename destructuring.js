c225410c1489a240f5bd0eaa4238658c94780758
Mira Jones <meschoolstuffya@gmail.com>
2018-02-28 15:49:52 -0700

    Finished

--- destructuring.js
+++ destructuring.js
@@ -22,6 +22,7 @@ var carDetails = {
 */
 
 //Code Here
+var {color, make, model, year} = carDetails;
 
 
 
@@ -35,7 +36,7 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
-  
+  var {firstName, lastName, title} = obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -54,7 +55,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function totalPopulation(obj){
+  var {utah, california, texas, arizona} = obj;
+  return utah + california + texas + arizona;
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -68,7 +72,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function ingredients(obj){
+  var {carb, fat, protein} = obj;
+  return [carb, fat, protein]
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -86,7 +93,9 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function largeNumbers({first, second, third}){
+  return Math.min(first, second, third)
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -98,5 +107,13 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function numberGroups({a, b, c}){
+  if(a.length>b.length && a.length > c.length){
+    return a
+  }else if(b.length> a.length && b.length>c.length){
+    return b
+  }else{
+    return c
+  }
+}
 
