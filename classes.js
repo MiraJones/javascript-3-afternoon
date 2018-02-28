d5d06a31ba2d67434370fbd15b931bc6efde8110
Mira Jones <meschoolstuffya@gmail.com>
2018-02-28 15:30:25 -0700

    Finished

--- classes.js
+++ classes.js
@@ -31,6 +31,19 @@
 
 //Code Here
 
+class Employee{
+  constructor(first_name, last_name, email, age ) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age;
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
+
 
 
 ////////// PROBLEM 2 //////////
@@ -50,6 +63,21 @@
 */
 
 //Code Here
+class Manager{
+  constructor(first_name, last_name, email, age){
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age;
+  this.reports = [];
+  }
+  hire(employee){
+    this.reports.push(employee);
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+  }
+}
 
 
 
@@ -76,7 +104,37 @@
 */
 
 //Code Here
-
+class ProgressiveManager{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+    this.title = "Not a manager";
+    this.bonus = 0;
+  }
+  hire(employee){
+    this.reports.push(employee);
+    if(this.reports.length===0){
+      this.title = "Not a manager"
+    }else if(this.reports.length<=3){
+      this.title = "Barely Manager"
+    }else if(this.reports.length<=10){
+      this.title = "Mostly Manager"
+    }else if(this.reports.length<=50){
+      this.title = "Manager"
+    }else if(this.reports.length<=100){
+      this.title = "Manager Plus"
+    }else{
+      this.title = "Bestest Manager"
+    }
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+    this.bonus += 100
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -103,5 +161,28 @@
 */
 
 //Code Here
-
-
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+    this.count = 0;
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    this.count += num;
+    while(this.count>=50){
+      this.wear_and_tear_count += 1
+      this.count-=50
+    }
+  }
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+  reboot(){
+    return (()=> {
+      this.wear_and_tear_count-=10
+      this.needs_reboot=false;
+    })
+  }
+}
